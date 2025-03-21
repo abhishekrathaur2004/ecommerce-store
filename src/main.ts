47010e2771db96edcbfd8149bf619700136d30b7
Abhishek Krishnan Rathaur <abhishek.krishnan@geekyants.com>
2025-03-22 02:20:15 +0530

    setup of swagger docs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Ecommerce App API')
+    .setDescription('The Ecommerce App API description')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  const port = process.env.PORT ?? 3000;
+  await app.listen(port, '0.0.0.0');
+  const appUrl = await app.getUrl();
+  Logger.log(`Server is running on ${appUrl}`);
 }
 bootstrap();
